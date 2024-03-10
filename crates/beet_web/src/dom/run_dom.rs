b7b57aee566436ce13ed287f45fe5ce905546b2e
Peter Hayman <peteyhayman@gmail.com>
2024-03-10 17:23:54 +1100

    remove: body.html

--- crates/beet_web/src/dom/run_dom.rs
+++ crates/beet_web/src/dom/run_dom.rs
@@ -8,7 +8,14 @@ pub fn append_html_for_tests() {
 	Document::x_head().append_child(&style_element).unwrap();
 
 	let root = Document::x_create_div();
-	root.set_inner_html(include_str!("body.html"));
+
+	let html = include_str!("../../assets/index.html");
+	let start_tag = "<body>";
+	let end_tag = "</body>";
+	let start_index = html.find(start_tag).unwrap() + start_tag.len();
+	let end_index = html.find(end_tag).unwrap();
+	let body_content = &html[start_index..end_index];
+
+	root.set_inner_html(body_content);
 	Document::x_body().append_child(&root).unwrap();
-	// Document::x_body().set_inner_html(include_str!("body.html"));
 }
