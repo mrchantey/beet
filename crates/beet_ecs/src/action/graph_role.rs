d19ee585168c5ad2d249628dbd03badf3bdb770c
Peter Hayman <peteyhayman@gmail.com>
2024-03-26 19:11:47 +1100

    Update graph_role.rs

--- crates/beet_ecs/src/action/graph_role.rs
+++ crates/beet_ecs/src/action/graph_role.rs
@@ -1,6 +1,6 @@
 /// Some extra metadata used to indicate the purpose of an action, ie which parts of the world it will effect.
 /// This is **not** used at runtime, only for UI and debugging purposes.
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum GraphRole {
 	/// This action will effect only this node
 	Node,
