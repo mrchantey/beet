c4abe39461cd21356ac9b1d6a81c62add5023ab2
Peter Hayman <peteyhayman@gmail.com>
2024-04-17 17:31:41 +1000

    patch

--- crates/beet_ecs/src/ecs_module/actions/mod.rs
+++ crates/beet_ecs/src/ecs_module/actions/mod.rs
@@ -1,9 +1,9 @@
 pub mod lifecycle_actions;
 #[allow(unused_imports)]
 pub use self::lifecycle_actions::*;
-pub mod succeed_in_duration;
-#[allow(unused_imports)]
-pub use self::succeed_in_duration::*;
 pub mod simple_actions;
 #[allow(unused_imports)]
 pub use self::simple_actions::*;
+pub mod insert_in_duration;
+#[allow(unused_imports)]
+pub use self::insert_in_duration::*;
