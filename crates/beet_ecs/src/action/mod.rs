a52a8e837d4a820f25bfe6e950b10472515baf50
Peter Hayman <peteyhayman@gmail.com>
2024-05-13 10:24:22 +1000

    patch

--- crates/beet_ecs/src/action/mod.rs
+++ crates/beet_ecs/src/action/mod.rs
@@ -4,9 +4,6 @@ pub use self::action_meta::*;
 pub mod action_plugin;
 #[allow(unused_imports)]
 pub use self::action_plugin::*;
-pub mod graph_role;
-#[allow(unused_imports)]
-pub use self::graph_role::*;
 pub mod action_systems;
 #[allow(unused_imports)]
 pub use self::action_systems::*;
