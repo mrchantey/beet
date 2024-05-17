066d4b896678f84c7ef6c0f6a19f3180eae1d6aa
Peter Hayman <peteyhayman@gmail.com>
2024-05-17 19:03:26 +1000

    remove: lightyear

--- crates/beet_net/src/lib.rs
+++ crates/beet_net/src/lib.rs
@@ -1,15 +1,1 @@
-// pub mod client;
-pub mod lightyear;
-// pub mod server;
-
-pub mod prelude {
-	// pub use crate::client::*;
-	pub use crate::lightyear::*;
-	// pub use crate::server::*;
-}
-
-
-pub mod exports {
-	pub use lightyear;
-	pub use lightyear_common;
-}
+pub mod prelude {}
