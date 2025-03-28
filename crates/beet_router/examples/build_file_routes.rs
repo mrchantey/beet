dd349af5183bef28dcc63e495034ed5c27f77609
Peter Hayman <peteyhayman@gmail.com>
2025-03-29 08:10:51 +1100

    fix: test route builder

--- crates/beet_router/examples/build_file_routes.rs
+++ crates/beet_router/examples/build_file_routes.rs
@@ -1,21 +1,40 @@
+use anyhow::Result;
 use beet_router::prelude::*;
-use beet_rsx::rsx::BuildStep;
+use beet_rsx::prelude::*;
 use sweet::prelude::ReadFile;
+use sweet::prelude::WorkspacePathBuf;
 
 /// Demonstration of how to collect all files in the 'routes' dir
 /// and create a `routes.rs` file containing them all.
-pub fn main() {
-	let out_file = "crates/beet_router/src/test_site/codegen/routes.rs";
-	let mut parser = BuildFileRoutes::http_routes(
-		"crates/beet_router/src/test_site/routes",
-		out_file,
-		"beet_router",
+pub fn main() -> Result<()> {
+	let out_file = WorkspacePathBuf::new(
+		"crates/beet_router/src/test_site/codegen/routes.rs",
 	);
-	parser.codegen_file.use_beet_tokens = "use beet_router::as_beet::*;".into();
-	parser.run().unwrap();
-	let file = ReadFile::to_string(out_file).unwrap();
+	FileGroup::new_workspace_rel("crates/beet_router/src/test_site/routes")?
+		.with_filter(
+			GlobFilter::default()
+				.with_include("*.rs")
+				.with_exclude("*mod.rs"),
+		)
+		.pipe(FileGroupToFuncFiles::default())?
+		.pipe(FuncFilesToRouteFuncs::http_routes())?
+		.pipe(RouteFuncsToCodegen::new(
+			CodegenFile::new_workspace_rel(
+				"crates/beet_router/src/test_site/codegen/routes.rs",
+				"beet_router",
+			)
+			.with_use_beet_tokens("use beet_router::as_beet::*;"),
+		))?
+		.map(|(_, routes, codegen)| -> Result<_> {
+			codegen.build_and_write()?;
+			Ok(routes)
+		})?;
+
+	let file =
+		ReadFile::to_string(out_file.into_canonical_unchecked()).unwrap();
 
 
 	// let routes = parser.build_strings().unwrap();
-	println!("wrote {}\n{}", out_file, file);
+	println!("wrote {}\n{}", out_file.display(), file);
+	Ok(())
 }
