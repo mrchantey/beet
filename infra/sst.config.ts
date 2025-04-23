754e7af0df15495251c198cb7ba37fe4877adb7e
Peter Hayman <peteyhayman@gmail.com>
2025-04-24 09:17:17 +1000

    lint

--- infra/sst.config.ts
+++ infra/sst.config.ts
@@ -1,7 +1,5 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
-
-
 export default $config({
   app(input) {
     return {
@@ -11,21 +9,20 @@ export default $config({
       home: "aws",
       providers: {
         aws: {
-          region: "us-west-2", 
-        }
-      }
+          region: "us-west-2",
+        },
+      },
     };
   },
   async run() {
-
     new sst.aws.Function("BeetSiteLambda", {
       // this name *must* match beet deploy --function-name ...
       name: "BeetSiteLambda",
       // the rust runtime is not ready, we deploy ourselves
-      runtime: 'rust',
+      runtime: "rust",
       // doesnt work but this is the one we want, we're not using
       // sst for lambda deployment
-      // runtime: "provided.al2023", 
+      // runtime: "provided.al2023",
       // what is this used for?
       handler: "./crates/beet_site/",
       url: true,
@@ -34,18 +31,18 @@ export default $config({
       permissions: [
         {
           actions: [
-        "s3:*"
+            "s3:*",
           ],
-          resources: ["*"]
+          resources: ["*"],
         },
         {
           actions: [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
           ],
-          resources: ["*"]
-        }
+          resources: ["*"],
+        },
       ],
     });
   },
