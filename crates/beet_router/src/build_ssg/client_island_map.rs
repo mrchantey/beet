9554494cb1cf4adcdfb6c1ea5e66502e6452ad35
Peter Hayman <peteyhayman@gmail.com>
2025-06-12 23:20:03 +1000

    remove: old code

--- crates/beet_router/src/build_ssg/client_island_map.rs
+++
@@ -1,8 +0,0 @@
-use crate::prelude::*;
-use beet_template::prelude::*;
-use rapidhash::RapidHashMap;
-
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
-pub struct ClientIslandMap {
-	pub map: RapidHashMap<RouteInfo, Vec<ClientIsland>>,
-}
