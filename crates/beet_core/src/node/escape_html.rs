f9a02c0817b74bd37c93e8380dcc8164c2cd1722
Peter Hayman <peteyhayman@gmail.com>
2025-08-03 11:43:28 +1000

    wip: code nodes

---
+++ crates/beet_core/src/node/escape_html.rs
@@ -0,0 +1,33 @@
+/// Wrapper struct which will emit the HTML-escaped version of the contained
+/// string when passed to a format string.
+pub struct Escape<'a>(pub &'a str);
+
+
+pub struct EscapeHtml;
+
+
+impl EscapeHtml {
+	/// Escape a string for use in HTML.
+	pub fn escape(s: &str) -> String {
+		let mut escaped = String::with_capacity(s.len());
+		for character in s.chars() {
+			match character {
+				'&' => escaped.push_str("&amp;"),
+				'<' => escaped.push_str("&lt;"),
+				'>' => escaped.push_str("&gt;"),
+				'"' => escaped.push_str("&quot;"),
+				'\'' => escaped.push_str("&#39;"),
+				_ => escaped.push(character),
+			}
+		}
+		escaped
+	}
+	/// Unescape a string from HTML.
+	pub fn unescape(s: &str) -> String {
+		s.replace("&amp;", "&")
+			.replace("&lt;", "<")
+			.replace("&gt;", ">")
+			.replace("&quot;", "\"")
+			.replace("&#39;", "'")
+	}
+}
