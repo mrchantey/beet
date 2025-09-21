cf7eab0c56600c689121495d48c1c046b600b9de
Pete Hayman <peteyhayman@gmail.com>
2025-09-21 19:19:34 +1000

    patch

--- crates/beet_utils/src/js_runtime.rs
+++ crates/beet_utils/src/js_runtime.rs
@@ -1,33 +1,31 @@
 use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 unsafe extern "C" {
-	#[wasm_bindgen]
 	/// Get the current working directory, ie `Deno.cwd()`
 	/// just like [`std::process::cwd`], this will be relative to the crate, ie
 	/// cargo test --workspace is different cwd from cargo test -p my_crate
-	pub fn cwd() -> String;
 	#[wasm_bindgen]
+	pub fn cwd() -> String;
 	/// Use this instead of `std::process::exit` which outputs
 	/// an unholy `Uncaught RuntimeError: unreachable`
+	#[wasm_bindgen]
 	pub fn exit(code: i32);
-	#[wasm_bindgen(catch)]
 	/// Just run the function outside of the wasm boundary
 	/// ie `const panic_to_error = (f)=>f()`
+	#[wasm_bindgen(catch)]
 	pub fn panic_to_error(
 		f: &mut dyn FnMut() -> Result<(), String>,
 	) -> Result<(), JsValue>;
-	#[wasm_bindgen]
 	/// Read a file from the filesystem, ie `Deno.read_file()`
+	#[wasm_bindgen]
 	pub fn read_file(path: &str) -> Option<String>;
-	#[wasm_bindgen]
 	/// Get the SWEET_ROOT env var, ie `Deno.env.get("SWEET_ROOT")`
-	pub fn sweet_root() -> Option<String>;
 	#[wasm_bindgen]
+	pub fn sweet_root() -> Option<String>;
 	/// Get single environment variable by key, ie `Deno.env.get(key)`
-	#[wasm_bindgen(js_name = env_var)]
+	#[wasm_bindgen]
 	pub fn env_var(key: &str) -> Option<String>;
-	// #[wasm_bindgen]
 	/// Get all environment variables as entries 2D array, ie `Object.entries(Deno.env.toObject())`
-	#[wasm_bindgen(js_name = env_all)]
+	#[wasm_bindgen]
 	pub fn env_all() -> js_sys::Array;
 }
