63854bfa8cb46e5178eac9da8df96c62bacfe1b7
Peter Hayman <peteyhayman@gmail.com>
2026-03-23 09:05:31 +1100

    remove: old implementation

--- crates/beet_clanker/src/lib.rs
+++ crates/beet_clanker/src/lib.rs
@@ -3,21 +3,19 @@
 #![cfg_attr(test, test_runner(beet_core::test_runner))]
 #![cfg_attr(feature = "nightly", feature(closure_track_caller))]
 
-// mod actions;
 pub mod document;
 pub mod o11s;
 mod providers;
 pub mod realtime;
+mod tool;
 mod types;
-// mod tools;
 
 
 pub mod prelude {
-	// pub use crate::actions::*;
-	// pub use crate::tools::*;
 	pub use crate::document::*;
 	pub use crate::o11s;
 	pub use crate::providers::*;
 	pub use crate::realtime;
+	pub use crate::tool::*;
 	pub use crate::types::*;
 }
--- crates/beet_clanker/src/tool/mod.rs
+++ crates/beet_clanker/src/tool/mod.rs
@@ -1,4 +1,2 @@
 mod tool_query;
-mod types;
 pub use tool_query::*;
-pub use types::*;
