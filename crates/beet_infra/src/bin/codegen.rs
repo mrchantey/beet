483b2220c33c89bbd953cf20b0512ed241f70e34
Peter Hayman <peteyhayman@gmail.com>
2026-03-31 22:53:57 +1100

    wip: codegen

---
+++ crates/beet_infra/src/bin/codegen.rs
@@ -0,0 +1,6 @@
+//! Generate commonly used types.
+//! Codegen relies on open-tofu installation and takes tens of seconds,
+//! so we commit common types to the respository
+
+
+fn main() {}
--- crates/beet_infra/src/lib.rs
+++ crates/beet_infra/src/lib.rs
@@ -3,5 +3,6 @@ mod types;
 
 pub mod prelude {
 	pub use crate::types::*;
-	pub const CHEESE: bool = true;
 }
+
+
