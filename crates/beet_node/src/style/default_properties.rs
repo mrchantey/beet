e90257acf072d91019c9fc4640496179ac24998e
Peter Hayman <peteyhayman@gmail.com>
2026-04-22 11:38:49 +1000

    patch

--- crates/beet_node/src/style/default_properties.rs
+++ crates/beet_node/src/style/default_properties.rs
@@ -2,11 +2,6 @@ use crate::prelude::*;
 use crate::style::PropertyMap;
 use beet_core::prelude::*;
 
-
-
-
-
-
 pub struct DefaultPropertySet {
 	// tags to include
 	include_tags: Vec<SmolStr>,
@@ -15,36 +10,46 @@ pub struct DefaultPropertySet {
 }
 
 impl DefaultPropertySet {
-	pub fn new(tag:impl Into<SmolStr>)->Self{
-		Self{
+	pub fn new(tag: impl Into<SmolStr>) -> Self {
+		Self {
 			include_tags: vec![tag.into()],
-			exclude_tags: Vec::new()
+			exclude_tags: Vec::new(),
+			property_map: PropertyMap::default(),
 		}
 	}
+
 	pub fn with(
 		mut self,
 		property: impl Into<Property>,
-		value: Token,
+		value: impl Into<Token>,
 	) -> Self {
-		self.0.insert(property.into(), value);
+		self.property_map.insert(property.into(), value.into());
 		self
-	}	
-	
+	}
+
 	pub fn passes(&self, el: &Element) -> bool {
 		(self.include_tags.is_empty()
 			|| self.include_tags.iter().any(|tag| tag == el.tag()))
 			&& !self.exclude_tags.iter().any(|tag| tag == el.tag())
 	}
+
+	pub fn property_map(&self) -> &PropertyMap { &self.property_map }
 }
 
-
+#[derive(Resource, Component)]
 pub struct DefaultPropertyMap(Vec<DefaultPropertySet>);
 
+impl DefaultPropertyMap {
+	pub fn new(sets: Vec<DefaultPropertySet>) -> Self { Self(sets) }
 
-pub fn baseline_default_properties()->DefaultPropertyMap{
-	DefaultPropertyMap(
-	vec![
+	pub fn iter(&self) -> impl Iterator<Item = &DefaultPropertySet> {
+		self.0.iter()
+	}
+}
+
+pub fn baseline_default_properties() -> DefaultPropertyMap {
+	DefaultPropertyMap::new(vec![
 		DefaultPropertySet::new("a")
-			.with(props::FOREGROUND_COLOR, colors::ON_PRIMARY)		
+			.with(&*props::FOREGROUND_COLOR, &*colors::ON_PRIMARY),
 	])
 }
